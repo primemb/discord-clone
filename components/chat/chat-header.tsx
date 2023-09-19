faa63379250c8b9f4aa22b314fd9729fd077eb0b
Prime <mo.behzad34@gmail.com>
2023-09-19 12:42:15 +0330

    added user avatar to chat header

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react";
 
 import MobileToggle from "@/components/mobile-toggle";
+import UserAvatar from "@/components/user-avatar";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -15,6 +16,9 @@ const ChatHeader = ({ name, serverId, type, imageUrl }: ChatHeaderProps) => {
       <MobileToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+      {type === "converstion" && (
+        <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
     </div>
