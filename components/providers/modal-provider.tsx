c27924741ee9976b2d89672d84a7587756941592
Prime <mo.behzad34@gmail.com>
2023-09-29 12:18:20 +0330

    add delete messages

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -11,6 +11,7 @@ import LeaveServerModal from "@/components/modals/leave-server-modal";
 import DeleteServerModal from "@/components/modals/delete-server-modal";
 import EditChannelModal from "@/components/modals/edit-channel-modal";
 import MessageFileModal from "@/components/modals/message-file-modal";
+import DeleteMessageModal from "@/components/modals/delete-message-modal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -32,6 +33,7 @@ const ModalProvider = () => {
       <DeleteServerModal />
       <EditChannelModal />
       <MessageFileModal />
+      <DeleteMessageModal />
     </>
   );
 };
