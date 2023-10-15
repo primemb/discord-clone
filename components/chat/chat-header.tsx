6532b09758806b851554412c86fa1cbc4672f7f6
Prime <mo.behzad34@gmail.com>
2023-10-15 18:51:58 +0330

    add voice call

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -3,6 +3,7 @@ import { Hash } from "lucide-react";
 import MobileToggle from "@/components/mobile-toggle";
 import UserAvatar from "@/components/user-avatar";
 import SocketIndicator from "@/components/socket-indicator";
+import { ChatVideoButton } from "./chat-video-button";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -23,6 +24,7 @@ const ChatHeader = ({ name, serverId, type, imageUrl }: ChatHeaderProps) => {
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
       <div className="ml-auto flex items-center">
+        {type === "converstion" && <ChatVideoButton />}
         <SocketIndicator />
       </div>
     </div>
